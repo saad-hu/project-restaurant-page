8dea619be02841197fc21c03cb367421f0a95ca2
Dodo <saad9191h@gmail.com>
2022-09-28 08:46:53 -0500

    added header code in index.js. also downloaded loaders for cc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('Hello World');
+
+let body = document.querySelector('body');
+//creating header element
+let header = document.createElement('header');
+header.className = 'header';
+
+//creating heading element and appending it to header
+let heading = document.createElement('div');
+heading.className = 'heading';
+heading.textContent = 'Bistecca'
+header.appendChild(heading);
+
+//creating navigation element
+let navigation = document.createElement('ul');
+navigation.className = 'navigation options';
+//this adds items to the the navigation menu
+for(let i = 0; i < 3; i++) {
+    let navItem = document.createElement('li');
+    navItem.className = 'nav-item';
+    //giving text to each item according to i
+    switch(i) {
+        case 0:
+            navItem.textContent = 'Home';
+            break;
+        case 1:
+            navItem.textContent = 'Menu';
+            break;
+        case 2:
+            navItem.textContent = 'Contact';
+            break;
+    }
+    navigation.appendChild(navItem);
+}
+//appending navigation to header
+header.appendChild(navigation);
+
+
+
+
+body.appendChild(header);
