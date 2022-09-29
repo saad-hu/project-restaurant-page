2f46ebb9f5a754942c0e8f3043099f91a0391b6a
Dodo <saad9191h@gmail.com>
2022-09-29 07:47:31 -0500

    created a seperate module for header and footer

---
+++ src/header-footer.js
@@ -0,0 +1,59 @@
+//this module exports a class. the class has methods which takes body(or any container) as an arguments. the method adds header or footer element to the body
+
+class createHeaderFooter {
+    // constructor(body) {
+    //     this.body = body;
+    // }
+
+    static createHeader(body) {
+
+        //creating header element
+        let header = document.createElement('header');
+        header.className = 'header';
+
+        //creating heading element and appending it to header
+        let heading = document.createElement('div');
+        heading.className = 'heading';
+        heading.textContent = 'Bistecca'
+        header.appendChild(heading);
+
+        //creating navigation element
+        let navigation = document.createElement('ul');
+        navigation.className = 'navigation options';
+        //this adds items to the the navigation menu
+        for (let i = 0; i < 3; i++) {
+            let navItem = document.createElement('li');
+            //giving text to each item according to i
+            switch (i) {
+                case 0:
+                    navItem.textContent = 'Home';
+                    navItem.className = 'nav-item home-tab-button';
+                    break;
+                case 1:
+                    navItem.textContent = 'Menu';
+                    navItem.className = 'nav-item menu-tab-button';
+                    break;
+                case 2:
+                    navItem.textContent = 'Contact';
+                    navItem.className = 'nav-item contact-tab-button';
+                    break;
+            }
+            navigation.appendChild(navItem);
+        }
+        //appending navigation to header
+        header.appendChild(navigation);
+
+        //appending complete header to body taken as argument
+        body.appendChild(header);
+    }
+
+    static createFooter(body) {
+        let footer = document.createElement('footer');
+        footer.className = 'footer';
+        footer.innerHTML = 'Copyright <a href="https://github.com/saad-hu">saad-hu</a>';
+        //appending complete footer to body taken as argument
+        body.appendChild(footer);
+    }
+}
+
+export default createHeaderFooter;
--- src/index.js
+++ src/index.js
@@ -1,49 +1,12 @@
 import './style.css';
 import createHomeTab from './home.js';
+import createHeaderFooter from './header-footer.js';
 
 //reference to body. body will have three elements appended to it: .header, .main-container, .footer
 let body = document.querySelector('body');
 
-//creating header element
-let header = document.createElement('header');
-header.className = 'header';
 
-//creating heading element and appending it to header
-let heading = document.createElement('div');
-heading.className = 'heading';
-heading.textContent = 'Bistecca'
-header.appendChild(heading);
-
-//creating navigation element
-let navigation = document.createElement('ul');
-navigation.className = 'navigation options';
-//this adds items to the the navigation menu
-for(let i = 0; i < 3; i++) {
-    let navItem = document.createElement('li');
-    //giving text to each item according to i
-    switch(i) {
-        case 0:
-            navItem.textContent = 'Home';
-            navItem.className = 'nav-item home-tab-button';
-            break;
-        case 1:
-            navItem.textContent = 'Menu';
-            navItem.className = 'nav-item menu-tab-button';
-            break;
-        case 2:
-            navItem.textContent = 'Contact';
-            navItem.className = 'nav-item contact-tab-button';
-            break;
-    }
-    navigation.appendChild(navItem);
-}
-//appending navigation to header
-header.appendChild(navigation);
-
-//appending header to body
-body.appendChild(header);
-
-
+createHeaderFooter.createHeader(body);
 
 
 
@@ -64,12 +27,4 @@ createHomeTab(mainContainer);
 
 
 
-
-
-
-
-
-let footer = document.createElement('footer');
-footer.className = 'footer';
-footer.innerHTML = 'Copyright <a href="https://github.com/saad-hu">saad-hu</a>';
-body.appendChild(footer);
+createHeaderFooter.createFooter(body);
