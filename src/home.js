81f08cfd5ab8bf9c1411eaa307853d8893a0e91d
Dodo <saad9191h@gmail.com>
2022-09-29 07:14:06 -0500

    created a seperate module for home tab

---
+++ src/home.js
@@ -0,0 +1,32 @@
+//this module exports a function which appends home tab elements, to the container given to it as an argument
+
+import steakGif from './images/steak.gif';
+
+//this function takes a container as an argument. all the elements created in the function, will be appended to this container 
+function createHomeTab(container) {
+    //refernce to home tab. this is the container in which all fo the sub elements will be appended to, such as paragraphs, images etc 
+    let homeTab = document.createElement('div');
+    homeTab.classList.add('home-tab');
+
+    //first para in home tab
+    let homeIntroPara = document.createElement('p');
+    homeIntroPara.classList.add('home-intro-para');
+    homeIntroPara.textContent = 'Bistecca brings you the ultimate world of flavors. A fine dining restaurant with excellent food, great ambience and exceptional service. Order your favorite dish online from Bistecca and witness the best gourmet experience.'
+    container.appendChild(homeIntroPara);
+
+    //image for home tab
+    let homeGif = new Image();
+    homeGif.classList.add('home-steak-gif');
+    homeGif.src = steakGif;
+    container.appendChild(homeGif);
+
+    //second para in home tab
+    let homeSincePara = document.createElement('p');
+    homeSincePara.textContent = 'Serving the best steaks in town since 1980';
+    container.appendChild(homeSincePara);
+
+    //appending the homeTab to the container taken as an input parameter
+    container.appendChild(homeTab);
+}
+
+export default createHomeTab;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './style.css';
-import steakGif from './images/steak.gif';
+import createHomeTab from './home.js';
 
-
-//code for header starts here
+//reference to body. body will have three elements appended to it: .header, .main-container, .footer
 let body = document.querySelector('body');
+
 //creating header element
 let header = document.createElement('header');
 header.className = 'header';
@@ -55,31 +55,18 @@ mainContainer.classList.add('main-container');
 let homeTabButton = document.querySelector('.home-tab-button');
 let menuTabButton = document.querySelector('.menu-tab-button');
 let contactTabButton = document.querySelector('.contact-tab-button');
-
-//refernce to home tab
-let homeTab = document.createElement('div');
-homeTab.classList.add('home-tab');
-
-let homeIntroPara = document.createElement('p');
-homeIntroPara.classList.add('home-intro-para');
-
-homeIntroPara.textContent = 'Bistecca brings you the ultimate world of flavors. A fine dining restaurant with excellent food, great ambience and exceptional service. Order your favorite dish online from Bistecca and witness the best gourmet experience.'
-
-mainContainer.appendChild(homeIntroPara);
-
-
-let homeGif = new Image();
-homeGif.classList.add('home-steak-gif');
-homeGif.src = steakGif;
-mainContainer.appendChild(homeGif);
-
-
-let homeSincePara = document.createElement('p');
-homeSincePara.textContent = 'Serving the best steaks in town since 1980';
-mainContainer.appendChild(homeSincePara);
-
 //appending main container to body
 body.appendChild(mainContainer);
+
+
+
+createHomeTab(mainContainer);
+
+
+
+
+
+
 
 
 let footer = document.createElement('footer');
