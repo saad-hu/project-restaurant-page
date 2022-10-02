c8024beb57d3364dd6310b9de8e3eb4c97bb8251
Dodo <saad9191h@gmail.com>
2022-10-02 11:00:09 -0500

    added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,6 +8,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
@@ -24,4 +26,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Bistecca',
+      favicon: './src/images/favicon-128x128.png',
+    }),
+  ],
 };
